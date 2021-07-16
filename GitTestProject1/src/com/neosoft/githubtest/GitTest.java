334d09dd1a97df57389416925a8d1b7e104e29f6
Devesh2908 <170120107176@git.org.in>
2021-07-16 18:58:46 +0530

    First commit

---
+++ GitTestProject1/src/com/neosoft/githubtest/GitTest.java
@@ -0,0 +1,17 @@
+package com.neosoft.githubtest;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+
+		String code="SE";
+		if (code=="SE")
+		{
+			System.out.println("SoftWare Engineer");
+		}
+		else {
+			System.out.println("Not A Software Engineer ");
+		}
+	}
+
+}
